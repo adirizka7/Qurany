2ab2779a9cdf6dd4f9183cd776fd27a633e67c3f
Adi Rizka <adi.rizka@tokopedia.com>
2020-05-04 12:01:20 +0700

    restate core behavior

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request
-from .util.surah import Surah, surah_mapping
+
+import os
+import yaml
 
 app = Flask(__name__)
 
+_surahs_folder = 'static/Surahs'
+_surahs_mapping = 'util/surahs.yaml'
+
+def setup_static(): 
+    surahs_file = open(_surahs_mapping)
+    surahs = yaml.safe_load(surahs_file)
+    surahs = [surah for surah in surahs['surah_name']]
+    return surahs
+
+surahs = setup_static()
+
 @app.route('/')
 def index():
-    surahs = [x for x in surah_mapping.keys()]
-    return render_template('index.html', surahs=surahs)
+    return render_template('index.html')
 
 @app.route('/generate_surah', methods=['POST'])
 def generate_surah():
     selected_surah = request.form.get('selected_surah')
-    start = int(request.form.get('start_ayah'))
-    end = int(request.form.get('end_ayah'))
-    surah = Surah(selected_surah)
+    selected_surah = selected_surah.split('. ')
 
-    return render_template('index.html', surah_path=surah.slice(start, end))
+    if len(selected_surah) != 2:
+        raise ValueError(f"Surah's name is not valid, got {selected_surah}")
+
+    selected_surah = int(selected_surah[0])
+
+    surah_path = os.path.join(_surahs_folder, f'{selected_surah:03d}.mp3')
+
+    print(surah_path)
+    return render_template('index.html', surah_path=surah_path)
+
+@app.context_processor
+def context_processor():
+    return dict(surahs=surahs)
