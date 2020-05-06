e613f36dc51a319621c9a8351298a2d39f9171d5
adirizka7 <adiunity@gmail.com>
2020-05-07 00:42:05 +0700

    add condition before set time

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import yaml
 
 app = Flask(__name__)
 
-_surahs_folder = 'static/Surahs'
-_surahs_mapping = 'util/surahs.yaml'
+SURAHS_FOLDER = 'static/Surahs'
+SURAHS_MAPPING = 'util/surahs.yaml'
 
-def setup_static(): 
-    surahs_file = open(_surahs_mapping)
+def setup_static():
+    surahs_file = open(SURAHS_MAPPING)
     surahs = yaml.safe_load(surahs_file)
     surahs = [surah for surah in surahs['surah_name']]
     return surahs
@@ -23,18 +23,21 @@ def index():
 @app.route('/generate_surah', methods=['POST'])
 def generate_surah():
     selected_surah = request.form.get('selected_surah')
+
+    # to get surah's id from '1. Al-Fatihah' format
     selected_surah = selected_surah.split('. ')
 
     if len(selected_surah) != 2:
-        raise ValueError(f"Surah's name is not valid, got {selected_surah}")
+        raise ValueError(f"Surah's id is not valid, got {selected_surah}")
 
     selected_surah = int(selected_surah[0])
-
-    surah_path = os.path.join(_surahs_folder, f'{selected_surah:03d}.mp3')
+    surah_path = os.path.join(SURAHS_FOLDER, f'{selected_surah:03d}.mp3')
 
     print(surah_path)
     return render_template('index.html', surah_path=surah_path)
 
 @app.context_processor
 def context_processor():
-    return dict(surahs=surahs)
+    return dict(
+        surahs=surahs
+    )
